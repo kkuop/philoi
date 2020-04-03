eb4907aba837a5875f893c58413ad70e8a430a4a
kkuop <60794860+kkuop@users.noreply.github.com>
2020-04-03 11:09:31 -0500

    update to the interest model so the foreign key constraint references the right model

--- PhiloiWebApp/Models/Interests.cs
+++ PhiloiWebApp/Models/Interests.cs
@@ -15,10 +15,10 @@ namespace PhiloiWebApp.Models
 
         [ForeignKey("User")]
         public int UserId { get; set; }
-        public User user { get; set; }
+        public User User { get; set; }
 
         [ForeignKey("Categories")]
         public int CategoriesId { get; set; }
-        public Interests categories { get; set; }
+        public Categories Category { get; set; }
     }
 }
