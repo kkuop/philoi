3ec3a668191e468542f5a9158d0f82c615036194
Nick Johnson <daddeus72@gmail.com>
2020-04-03 11:13:33 -0500

    init branch commit2

--- PhiloiWebApp/Data/ApplicationDbContext.cs
+++ PhiloiWebApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PhiloiWebApp.Models;
@@ -20,13 +21,25 @@ namespace PhiloiWebApp.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Categories>()
               .HasData(
-                 new Categories { CategoriesId = 1, Name = "Fandoms"},
+                 new Categories { CategoriesId = 1, Name = "Fandoms" },
                  new Categories { CategoriesId = 2, Name = "Sports" },
                  new Categories { CategoriesId = 3, Name = "Music" },
                  new Categories { CategoriesId = 4, Name = "Activites" },
                  new Categories { CategoriesId = 5, Name = "Movies" }
               );
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole 
+            { Name = "Admin", NormalizedName = "ADMIN" 
+            
+            },
+                new IdentityRole { Name = "User", NormalizedName = "USER"}
+                );
         }
+      
+    
+
+    
 
         public DbSet<User> User { get; set; }
         public DbSet<Interests> Interests { get; set; }
