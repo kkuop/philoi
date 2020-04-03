a622d0c1de1e90a5a4da573e7dee42b6a7199076
kkuop <60794860+kkuop@users.noreply.github.com>
2020-04-03 15:24:31 -0500

    created RepositoryBase class with methods from contract

---
+++ PhiloiWebApp/Abstract Classes/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhiloiWebApp.Contracts;
+using PhiloiWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PhiloiWebApp.Abstract_Classes
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected ApplicationDbContext ApplicationDbContext { get; set; }
+        public RepositoryBase(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+        public IQueryable<T> FindAll()
+        {
+            return ApplicationDbContext.Set<T>().AsNoTracking();
+        }
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return ApplicationDbContext.Set<T>().Where(expression).AsNoTracking();
+        }
+        public void Create(T entity)
+        {
+            ApplicationDbContext.Set<T>().Add(entity);
+        }
+        public void Update(T entity)
+        {
+            ApplicationDbContext.Set<T>().Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            ApplicationDbContext.Set<T>().Remove(entity);
+        }
+    }
+}
