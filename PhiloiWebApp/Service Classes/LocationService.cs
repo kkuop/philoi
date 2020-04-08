cfc81a517a03b93b75c68fd722fa9bf702f750b3
Nick Johnson <daddeus72@gmail.com>
2020-04-08 08:40:28 -0500

    Distance method added. user properties where changed to better feit the api needs

--- PhiloiWebApp/Service Classes/LocationService.cs
+++ PhiloiWebApp/Service Classes/LocationService.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using PhiloiWebApp.Models;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PhiloiWebApp.Service_Classes
@@ -12,7 +12,18 @@ namespace PhiloiWebApp.Service_Classes
         
         
         }
+        
+        public async Task<LocationService> GetDistance(User user1,User user2)
+        {
 
-
+            HttpClient client = new HttpClient();
+            HttpResponseMessage httpResponse = await client.GetAsync($"https://maps.googleapis.com/maps/api/directions/json?origin={user1.Address}&destination={user2.Address}&key={ApiKey.GoogleKey}");
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                string json = httpResponse.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<LocationService>(json);
+            }
+            return null;
+        }
     }
 }
